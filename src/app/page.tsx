ebae79989a9faa024eb21ea3bc4e3be110a0f388
Abul Kashem Lukman <abulkashemlukman@gmail.com>
2025-06-11 11:04:19 +0100

    configuration[done]

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button>Outline</Button>
+    </div>
+  )
+}
+
+export default HomePage
